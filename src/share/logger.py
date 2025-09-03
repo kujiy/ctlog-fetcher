c61548fdd10e149a851637e22e94bf73315e717c
kujiy <kujiy.fs@gmail.com>
2025-09-03 11:17:01 +0900

    uvicorn.access logger also outputs datetime

--- src/share/logger.py
+++ src/share/logger.py
@@ -13,3 +13,10 @@ handler.setFormatter(formatter)
 
 logger.addHandler(handler)
 
+
+## Uvicorn
+# Ensure uvicorn.access logger also outputs datetime
+_uvicorn_access_logger = logging.getLogger("uvicorn.access")
+for h in _uvicorn_access_logger.handlers:
+    h.setFormatter(logging.Formatter("%(asctime)s %(levelname)s: %(message)s"))
+    #h.setFormatter(logging.Formatter("%(asctime)s [pid=%(process)d] [thread=%(threadName)s] %(levelname)s: %(message)s"))
