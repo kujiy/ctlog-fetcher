d9ddcfcac11bd001fd87e4fe26b78d5b25a7f3b3
kujiy <kujiy.fs@gmail.com>
2025-09-04 18:06:02 +0900

    bigint

--- dump/modify/ptosc_worker_status_bigint.sh
+++ dump/modify/ptosc_worker_status_bigint.sh
@@ -1,19 +1,27 @@
 #!/bin/bash
 # WorkerStatusテーブルのstart, end, current, last_uploaded_indexをBIGINTにオンラインで変更
-# レプリカ遅延監視を無効化 (--check-slave-lag=no) でエラー回避
 
-PT="pt-online-schema-change"
-HOST="192.168.0.180"
-USER="root"
-DB="ct"
-TABLE="worker_status"
+pt-online-schema-change \
+--alter "MODIFY COLUMN start BIGINT, MODIFY COLUMN end BIGINT, MODIFY COLUMN current BIGINT, MODIFY COLUMN last_uploaded_index BIGINT" \
+--chunk-index=PRIMARY \
+--chunk-time=0.5 \
+--nocheck-unique-key-change \
+--skip-check-replica-lag \
+--skip-check-slave-lag \
+--recursion-method=none \
+--set-vars "lock_wait_timeout=5,innodb_lock_wait_timeout=5" \
+--dry-run \
+"h=192.168.0.180,D=ct,t=worker_status,u=root,p="
 
-$PT --alter "MODIFY COLUMN start BIGINT, MODIFY COLUMN end BIGINT, MODIFY COLUMN current BIGINT, MODIFY COLUMN last_uploaded_index BIGINT" \
-  --chunk-index=PRIMARY \
-  --chunk-time=0.5 \
-  --nocheck-unique-key-change \
-  --set-vars "lock_wait_timeout=5,innodb_lock_wait_timeout=5" \
-  --no-check-slave-lag \
-  --recursion-method=none 
-  --execute \
-  "h=$HOST,D=$DB,t=$TABLE,u=$USER"
+pt-online-schema-change \
+--alter "MODIFY COLUMN start BIGINT, MODIFY COLUMN end BIGINT, MODIFY COLUMN current BIGINT, MODIFY COLUMN last_uploaded_index BIGINT" \
+--chunk-index=PRIMARY \
+--chunk-time=0.5 \
+--nocheck-unique-key-change \
+--skip-check-replica-lag \
+--skip-check-slave-lag \
+--recursion-method=none \
+--set-vars "lock_wait_timeout=5,innodb_lock_wait_timeout=5" \
+--execute \
+"h=192.168.0.180,D=ct,t=worker_status,u=root,p="
+
