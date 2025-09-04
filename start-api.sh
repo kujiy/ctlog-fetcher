ba825ba2a3c0a1935cc727964602d25e5fc9bb4c
kujiy <kujiy.fs@gmail.com>
2025-09-04 11:23:11 +0900

    start param

--- start-api.sh
+++ start-api.sh
@@ -1,3 +1,5 @@
 
 . venv/bin/activate
-uvicorn src.manager_api.main:app --port 1173 --host 0.0.0.0 --workers 12
+
+#uvicorn src.manager_api.main:app --port 1173 --host 0.0.0.0 --workers 12
+mkdir -p /tmp/metrics;rm -rf /tmp/metrics/*;  nohup env PROMETHEUS_MULTIPROC_DIR=/tmp/metrics uvicorn src.manager_api.main:app --port 1173 --host 0.0.0.0 --workers 3 &
