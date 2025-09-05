5e85a9a3bae13b35e2c3a4996dec8c07fecf1a85
kujiy <kujiy.fs@gmail.com>
2025-09-05 22:29:14 +0900

    not started

--- src/manager_api/main.py
+++ src/manager_api/main.py
@@ -34,7 +34,7 @@ from prometheus_client import CollectorRegistry, multiprocess, generate_latest, 
 from .background_jobs.sth_fetcher import start_sth_fetcher
 from .background_jobs.worker_liveness import start_worker_liveness_monitor
 from .background_jobs.log_fetch_progress import start_log_fetch_progress
-from .background_jobs.unique_cert_counter import start_unique_cert_counter_sqlite_counter, get_unique_cert_counter_count
+from .background_jobs.unique_cert_counter import start_unique_cert_counter, get_unique_cert_counter_count
 
 
 # JST timezone
@@ -127,7 +127,7 @@ async def on_startup():
             app.state.background_tasks = []
             app.state.background_tasks.append(start_sth_fetcher())
             app.state.background_tasks.append(start_worker_liveness_monitor())
-            app.state.background_tasks.append(start_unique_cert_counter_sqlite_counter())
+            app.state.background_tasks.append(start_unique_cert_counter())
             app.state.background_tasks.append(start_log_fetch_progress())
             logger.info("Background jobs started and tasks stored in app.state.background_tasks")
             app.state.background_jobs_lock_file = lock_file
