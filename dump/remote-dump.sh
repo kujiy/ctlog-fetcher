bce1283141b03d50d9dfb7a53efb69dbb6ad61dd
kujiy <kujiy.fs@gmail.com>
2025-09-06 14:38:43 +0900

    remote dump

--- dump/remote-dump.sh
+++ dump/remote-dump.sh
@@ -1,3 +1,2 @@
-ssh shinichi@192.168.0.180 \
-  "mysqldump -uroot --single-transaction --master-data=2 --databases ct | gzip -c" \
-  > ct_$(date +%Y%m%d).sql.gz
+mysqldump -h 192.168.0.180 -u root --single-transaction --master-data=2 --databases ct \
+| mysql -u root
